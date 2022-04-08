312fd4e48623c61eba2d9e4d0e906fa4e53a212d
arthurmoyo <93607076+arthurmoyo@users.noreply.github.com>
2022-04-08 05:17:20 +0200

    typo changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(requireHTTPS);
 app.use(express.static('./dist/daisy'));
 
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/daisy>/'}
+  res.sendFile('index.html', {root: 'dist/daisy'}
 );
 });
 
